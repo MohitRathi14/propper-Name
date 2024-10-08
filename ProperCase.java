c56814809d89be1443d25c2159b393ad78db3a3a
MohitRathi14 <aaditya375singh2@gmail.com>
2024-10-08 10:48:06 +0530

    Update ProperCase.java
    to chack the name proper case or not

--- ProperCase.java
+++ ProperCase.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class propercase2 {
+public class ProperCase {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String name = sc.nextLine();
