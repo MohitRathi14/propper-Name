17736c23f834ecfcb7a529e4d38ed6e9b6ff1a05
MohitRathi14 <aaditya375singh2@gmail.com>
2024-10-08 10:43:19 +0530

    ProperCase.java

--- ProperCase.java
+++ ProperCase.java
@@ -1,28 +1,28 @@
 import java.util.Scanner;
 
-public class ProperCase {
+public class propercase2 {
     public static void main(String[] args) {
-        Scanner sc =new Scanner(System.in);
-        String name =sc.nextLine();
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
 
-        name=name.toLowerCase();
-        name=name.trim();
-        String firstChar=(name.charAt(0)+"").toUpperCase();
+        name = name.toLowerCase();
+        name = name.trim().replaceAll("\\s+", " ");
+
         System.out.println(name);
-        for (int i = 1; i < name.length(); i++) {
-            int j=i;
-            while (name.charAt(i)==' ') {
-                
-                if (name.charAt(j)!= ' ') {
-                    name=name.substring(1, i+1)+(name.charAt(j)+"").toUpperCase()+name.substring(j+1);  
-                }
-                j++;
-                
-                
+
+        StringBuilder sb = new StringBuilder(name);
+        boolean capitalizeNext = true;
+
+        for (int i = 0; i < sb.length(); i++) {
+            if (sb.charAt(i) == ' ') {
+                capitalizeNext = true;
+            } else if (capitalizeNext) {
+                sb.setCharAt(i, Character.toUpperCase(sb.charAt(i)));
+                capitalizeNext = false;
             }
         }
-        System.out.println(firstChar+name);
+
+        System.out.println(sb.toString());
         sc.close();
-        
     }
 }
